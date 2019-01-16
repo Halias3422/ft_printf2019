9e718b62081a5f8a7a0bb38dd86896066f3667b3
Vivien De saint-exupery <vde-sain@z3r7p4.le-101.fr>
2019-01-16 14:05:01 +0100

    removed last leaks and invalid reads but it was weird

--- libftprintf/ft_strcpy.c
+++ libftprintf/ft_strcpy.c
@@ -6,7 +6,7 @@
 /*   By: vde-sain <marvin@le-101.fr>                +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/10/03 18:44:17 by vde-sain     #+#   ##    ##    #+#       */
-/*   Updated: 2018/10/11 11:22:14 by vde-sain    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/01/16 14:03:53 by vde-sain    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -18,7 +18,7 @@ char	*ft_strcpy(char *dst, const char *src)
 	int		b;
 
 	b = 0;
-	while (src[b])
+	while (src[b] < '\0')
 	{
 		dst[b] = src[b];
 		b++;
