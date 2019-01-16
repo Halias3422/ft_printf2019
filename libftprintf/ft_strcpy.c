f36d89f39466295e7771218b49db8ac9a378f8ae
Vivien De saint-exupery <vde-sain@z3r7p4.le-101.fr>
2019-01-16 16:26:53 +0100

    removed printf from ft_strcpy

--- libftprintf/ft_strcpy.c
+++ libftprintf/ft_strcpy.c
@@ -6,7 +6,7 @@
 /*   By: vde-sain <marvin@le-101.fr>                +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/10/03 18:44:17 by vde-sain     #+#   ##    ##    #+#       */
-/*   Updated: 2019/01/16 15:04:04 by vde-sain    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/01/16 16:26:05 by vde-sain    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -19,7 +19,6 @@ char	*ft_strcpy(char *dst, const char *src)
 	int		b;
 
 	b = 0;
-	printf("len src = %zu\n", ft_strlen(src)), fflush(stdout);
 	while (src[b])
 	{
 		dst[b] = src[b];
